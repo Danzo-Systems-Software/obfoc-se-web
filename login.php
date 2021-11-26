89c4911fcc7e1f8ec59495387ca475bd121ebfb7
Hirek <polomarket73@gmail.com>
2021-11-26 22:32:23 +0100

    Emptied login.php for future selecting login mehod

--- login.php
+++ login.php
@@ -1,28 +1,3 @@
 <?php
 
-
-
-if ($_GET["logout"] == "1"){
-    session_destroy();
-
-    $_SESSION["isdestroyed"] = true;
-    header('Location: '."index.php");
-    exit();
-}
-if($_GET["logout"]!="1"){
-    session_start();
-    $_SESSION['username'] = "Hirek";
-}
-if ($_GET["username"] == NULL){
-    header('Location: '."index.php");
-    exit();
-}
-
-
-//header('Location: '."index.php");
-
-
-
-
-
 ?>
